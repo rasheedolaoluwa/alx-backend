d1e68a8eb47fb9f164505ee86de28cb072a3f191
Rasheed <radeyola@gmail.com>
2024-08-20 19:58:10 +0000

    lfu cache +

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+""" LFU Caching """
+
+from base_caching import BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """ LFU caching implementation """
+
+    def __init__(self):
+        """ Initialize the cache """
+        super().__init__()
+        self.queue = []
+        self.counter = {}
+
+    def put(self, key, item):
+        """ Add an item to the cache """
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+
+        item_count = self.counter.get(key, None)
+
+        if item_count is not None:
+            self.counter[key] += 1
+        else:
+            self.counter[key] = 1
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first = self.get_first_list(self.queue)
+            if first:
+                self.queue.pop(0)
+                del self.cache_data[first]
+                del self.counter[first]
+                print("DISCARD: {}".format(first))
+
+        if key not in self.queue:
+            self.queue.insert(0, key)
+        self.mv_right_list(key)
+
+    def get(self, key):
+        """ Retrieve an item from the cache """
+        item = self.cache_data.get(key, None)
+        if item is not None:
+            self.counter[key] += 1
+            self.mv_right_list(key)
+        return item
+
+    def mv_right_list(self, item):
+        """ Move an element to the right, considering LFU """
+        length = len(self.queue)
+
+        idx = self.queue.index(item)
+        item_count = self.counter[item]
+
+        for i in range(idx, length):
+            if i != (length - 1):
+                nxt = self.queue[i + 1]
+                nxt_count = self.counter[nxt]
+
+                if nxt_count > item_count:
+                    break
+
+        self.queue.insert(i + 1, item)
+        self.queue.remove(item)
+
+    @staticmethod
+    def get_first_list(array):
+        """ Get the first element of a list or None """
+        return array[0] if array else None
