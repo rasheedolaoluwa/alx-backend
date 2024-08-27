16ca1a7bf4a74b018bf2ae4a85e536eba36dc907
Rasheed <radeyola@gmail.com>
2024-08-27 21:23:42 +0000

    force

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+Use Babel to get user locale.
+"""
+
+from flask_babel import Babel
+from flask import Flask, render_template, request
+
+app = Flask(__name__, template_folder='templates')
+babel = Babel(app)
+
+
+class Config(object):
+    """
+    Babel configuration settings.
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def helloWorld() -> str:
+    """
+    Render template with Babel integration.
+    """
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Determine and return the user locale for translation.
+    """
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run()
